58265cb636582184bc5e336141e8088007b8780e
David Rubio <davidrubiobenito@gmail.com>
2019-10-19 16:18:59 +0200

    VIDEO-13: patrones de diseño SINGLETON y PROTOTYPE

---
+++ src/com/drbotro/ioc/UsoDemoSingletonPrototype.java
@@ -0,0 +1,33 @@
+package com.drbotro.ioc;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UsoDemoSingletonPrototype{
+
+    public static void main(String[] args){
+
+        // 1. Cargar el archivo XML
+        ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext2.xml");
+
+        // 2. petición de bean al contenedor
+        SecretarioEmpleado maria = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+        SecretarioEmpleado pedro = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+        SecretarioEmpleado david = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+        SecretarioEmpleado ana = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+        SecretarioEmpleado manolo = contexto.getBean("miSecretarioEmpleado", SecretarioEmpleado.class);
+
+        System.out.println(maria);
+        System.out.println(pedro);
+        System.out.println(david);
+        System.out.println(ana);
+        System.out.println(manolo);
+
+        if(maria == pedro){
+            System.out.println("Maria y Pedro son los mismos objetos");
+        }else{
+            System.out.println("No son los mismos objetos");
+        }
+
+    }
+
+}
